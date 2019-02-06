fe7418de37a07593433311b2cb161641dd66932f
Bharath Dasaraju <bharath.dasaraju@delivion.de>
2019-02-06 05:44:31 +0100

    Sample Ghost game for python beginners

--- Book1-computer_coding/Chapter-1-turtle/1.helo-hello-helllooo.py
+++ Book1-computer_coding/Chapter-1-turtle/1.helo-hello-helllooo.py
@@ -1,3 +1,21 @@
+from random import randint
 print()
 print('hello world!')
+print(randint(1,5))
+def spaces_for_life():
+    for i in range(3):
+        print()
 
+spaces_for_life()
+feel_brave = True
+while feel_brave:
+    ghost_door = randint(1,5)
+    door = input('1, 2, 3, 4, 5 ...........?')
+    door_num = int(door)
+    if door_num == ghost_door:
+        print('Ghost Ghost Ghost .................')
+        print('Have Fun lol :)')
+        feel_brave = False
+    else:
+        print('Enter next room')
+print("Run Away!!!")

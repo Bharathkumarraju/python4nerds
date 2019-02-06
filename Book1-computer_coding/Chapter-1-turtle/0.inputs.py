d8604c7af7768e18601a93cfd3691f18ffe9ac30
Bharath Dasaraju <bharath.dasaraju@delivion.de>
2019-02-06 08:20:23 +0100

    just practise python

--- Book1-computer_coding/Chapter-1-turtle/0.inputs.py
+++ Book1-computer_coding/Chapter-1-turtle/0.inputs.py
@@ -6,3 +6,26 @@ spaces()
 age = input('whats the age?')
 print("The age is ", age)
 spaces()
+
+
+a = 2.1
+
+if a == 2:
+    print("Hello")
+else:
+    print("good bye!!!")
+    print("")
+
+for i in range(10):
+    print("SRi Anjaneyam!!! Prasanna Anjaneyam!!!")
+
+spaces()
+
+word='dragon'
+
+if word == 'dragon':
+    print("you are from china")
+elif word == 'elephant':
+    print("you are from india")
+else:
+    print("I don't know from which fucking country you are!!!")

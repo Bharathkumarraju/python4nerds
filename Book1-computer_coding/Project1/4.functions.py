db61cb2fa070c7e480b9ebc74be9836cc60c546e
Bharathkumar <bhrth.dsra1@gmail.com>
2019-03-05 14:57:28 +0100

    How to write funnctions in python with args with default values and annotations as well

---
+++ Book1-computer_coding/Project1/4.functions.py
@@ -0,0 +1,15 @@
+def greet(who:str='bharathkumar') -> str:
+    print("Hello  Mr. " + who)
+
+
+greet('Roopa Meesala')
+greet()
+
+
+def greet1(name:str='hanuman'):
+    print("Good After Noon " + name)
+
+print("")
+greet1()
+greet1('Sri Anjaneyam')
+

8de126b56cd312ea438214f688359d9774daf001
Bharathkumar <bhrth.dsra1@gmail.com>
2019-03-06 10:30:06 +0100

    How to append strings and lists some examples

---
+++ Book1-computer_coding/Project1/7.simple_join_strings.py
@@ -0,0 +1,30 @@
+print('join  strings like below')
+print("")
+print('greetinga'+'Bhajrang')
+
+hanuman = 'Jai Bhajrang Bhali'
+print("")
+print(hanuman[0])
+print(hanuman[6])
+
+print(len(hanuman))
+
+menu = list()
+
+menu.append('Idli')
+menu.append('vadai')
+menu.append('Rice')
+menu.append('Bhojanam')
+menu.append('hello good bye')
+
+print(menu)
+print(len(menu))
+
+del menu[1]
+print(menu)
+
+if 'Rice123' in menu:
+    print('You are from andhra pradesh')
+else:
+    print("You are from Punjab!!!")
+

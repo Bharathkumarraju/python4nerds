90ae61bc566a16386447be99e61354492a697364
Bharathkumar <bhrth.dsra1@gmail.com>
2019-03-06 17:12:37 +0100

    convert data types at ease using python

--- Book1-computer_coding/Project1/8.all_about_variables.py
+++ Book1-computer_coding/Project1/8.all_about_variables.py
@@ -11,7 +11,8 @@ print("")
 print(sheep, sheep_weight)
 print("")
 
-# Strings includes letters, numbers, spaces, and symbols such as full stops and commas. They are usually put inside single quote marks.
+# Strings includes letters, numbers, spaces, and symbols such as full stops and commas.
+# They are usually put inside single quote marks.
 
 a = "Coding is fun! isn't it?"
 print(a)
@@ -27,3 +28,17 @@ print(type(24.5609234))
 print(type(x))
 print(type(False))
 print("")
+
+# Converting Data Types
+# So variable contain any type of data , Problems occur if you try to mix types together.
+# Data types sometimes have to be converted, otherwise an error message will appear.
+
+apple = input('Enter number of apples')
+print("")
+print(type(apple))
+try:
+    print(apple + 1)
+except Exception as err:
+    print(err)
+# In order to fix above first convert apple to int as below
+print(int(apple) + 18)

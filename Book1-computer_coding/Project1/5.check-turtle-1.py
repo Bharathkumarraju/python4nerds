14862591a81500802871eaf63846f2eb2ccd6644
Bharathkumar <bhrth.dsra1@gmail.com>
2019-03-06 10:07:05 +0100

    Play with python graphics

---
+++ Book1-computer_coding/Project1/5.check-turtle-1.py
@@ -0,0 +1,14 @@
+from turtle import *
+import time
+
+color('red', 'yellow')
+begin_fill()
+pendown()
+time.sleep(2)
+forward(100)
+time.sleep(3)
+right(90)
+time.sleep(4)
+penup()
+end_fill()
+time.sleep(5)

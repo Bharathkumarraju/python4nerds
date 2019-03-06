6bc1887376c72be83a5e927d14233a7181844a9c
Bharathkumar <bhrth.dsra1@gmail.com>
2019-03-06 16:47:37 +0100

    All about data types integers floats strings and booleans

---
+++ Book1-computer_coding/Project1/8.all_about_variables.py
@@ -0,0 +1,29 @@
+rabbits = 5
+hats = rabbits
+
+print("")
+print(rabbits, hats)
+
+sheep = 10
+sheep_weight = 10.0123
+
+print("")
+print(sheep, sheep_weight)
+print("")
+
+# Strings includes letters, numbers, spaces, and symbols such as full stops and commas. They are usually put inside single quote marks.
+
+a = "Coding is fun! isn't it?"
+print(a)
+
+x = True
+y = False
+print(x , y)
+print("")
+
+print(type(24))
+print(type('24'))
+print(type(24.5609234))
+print(type(x))
+print(type(False))
+print("")

5a965404b45b7e58379e7e9c312a899893024aa7
Bharathkumar <bhrth.dsra1@gmail.com>
2019-03-07 13:43:28 +0100

    How String manipulation works in Python programming

---
+++ Book1-computer_coding/Project1/9.How_strings_behave.py
@@ -0,0 +1,16 @@
+a = "Run!"
+b = "Aliens are coming!!!"
+print("")
+
+c = a + b
+print(c)
+print("")
+
+d = a + 'Watch Out Please!!!' + b
+print(d)
+print("")
+
+e = b + '  Please Watch Out!!!  ' + a
+print(e)
+
+#print( a + b )

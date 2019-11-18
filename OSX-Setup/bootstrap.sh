c4d5fb3d0be81d6f6e269b928e60c3d7d3337a0c
Bharathkumar <bharath@hooq.tv>
2019-11-18 13:24:37 +0800

    updatge

--- OSX-Setup/bootstrap.sh
+++ OSX-Setup/bootstrap.sh
@@ -1,33 +1,4 @@
 #!/usr/bin/env bash
-#
-# (c) 2018, Bharath <bhrth.dsra1@gmail.com>
-# This module is part of the automation setup project of new OSX machines
-# done on the top of the following Gist:
-#
-# https://gist.github.com/codeinthehole/26b37efa67041e1307db
-#
-# Bootstrap script for setting up a new OSX machine
-#
-# This should be idempotent so it can be run multiple times.
-#
-# Some apps don't have a cask and so still need to be installed by hand. These
-# include:
-#
-# - Twitter (app store)
-# - Postgres.app (http://postgresapp.com/)
-#
-# Notes:
-#
-# - If installing full Xcode, it's better to install that first from the app
-#   store before running the bootstrap script. Otherwise, Homebrew can't access
-#   the Xcode libraries as the agreement hasn't been accepted yet.
-#
-# Reading:
-#
-# - http://lapwinglabs.com/blog/hacker-guide-to-setting-up-your-mac
-# - https://gist.github.com/MatthewMueller/e22d9840f9ea2fee4716
-# - https://news.ycombinator.com/item?id=8402079
-# - http://notes.jerzygangi.com/the-best-pgp-tutorial-for-mac-os-x-ever/
 
 echo "Starting bootstrapping"
 
